3de8ff599f94a439d874ddc629066c6227c13fd5
consultor.jean <consultor.jean@leomadeiras.com.br>
2022-06-21 12:12:46 -0300

    fix: correcting styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,3 @@
-import { Text } from '@chakra-ui/react'
 import { Contacts } from './Contacts'
 import { Highlights } from './Highlights'
 import { Informations } from './Informations'
@@ -20,7 +19,7 @@ export const Footer = () => (
 
     <S.Divider />
 
-    <S.CopyrightText>Copyright 2022 Todos os direitos reservados</S.CopyrightText>
+    <S.CopyrightText>Copyright ©2022 Todos os direitos reservados</S.CopyrightText>
     <S.CompanyText>Desenvolvido por tech architecture</S.CompanyText>
   </S.Container>
 )
